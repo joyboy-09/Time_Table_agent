89ec6d977e7215296bc8a5ae4e881220b80e43c1
JAMILI <jamiligowrish37@gmail.com>
2026-08-02 22:56:22 +0530

    Export Express app directly as default export in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const expressApp = require('../server/index.cjs');
+const app = require('../server/index.cjs');
 
-export default function handler(req, res) {
-  const fn = typeof expressApp === 'function' ? expressApp : (expressApp.default || expressApp);
-  fn(req, res);
-}
+export default app;
