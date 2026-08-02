f4cdfb4d11ddf541a5f83b17c6504c8287f6f95c
JAMILI <jamiligowrish37@gmail.com>
2026-08-02 22:53:04 +0530

    Use createRequire in api/index.js for Vercel serverless function

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const app = require('../server/index.cjs');
+
+export default function handler(req, res) {
+  app(req, res);
+}
