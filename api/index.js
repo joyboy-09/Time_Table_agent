9ea7b17e75d523245ad64ccec6e5aab1fb205667
JAMILI <jamiligowrish37@gmail.com>
2026-08-02 22:49:56 +0530

    Fix interop for CommonJS Express app import in ESM api/index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
-import app from '../server/index.cjs';
+import expressApp from '../server/index.cjs';
+
+const app = expressApp.default || expressApp;
 
 export default function handler(req, res) {
   app(req, res);
